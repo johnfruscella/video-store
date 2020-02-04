3d39590d8db30f01f1f5d26ccc7ecfb9c254432c
johnfruscella <johnfruscella@gmail.com>
2020-02-03 20:15:29 -0500

    Added models folder for genre & customer. Added validation for user inputs

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
+const object = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+mongoose.connect('mongodb://localhost/vidly', object)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
